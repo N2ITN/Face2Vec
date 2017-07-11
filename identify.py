b382a0594c9790ba5d38b8616fca24ec0057e73f
Zach <zach@livestories.com>
2017-07-10 20:11:31 -0700

    Docstrings and performance tweaks

--- identify.py
+++ identify.py
@@ -8,12 +8,16 @@ from sklearn.preprocessing import MaxAbsScaler
 
 class img_keypoints():
 
-    def __init__(self, pic):
+    def __init__(self, pic, draw=True):
+
+        self.draw = draw
         self.img = cv2.imread(pic)
         self.img_name = pic
-        assert self.img.shape[0] > 10
+
+        if not hasattr(self.img, 'nbytes'):
+
+            raise Exception('Not a valid image path.')
         self.detect_face()
-        self.crop_image()
         self.detect_keypoints()
         self.centerPoint = self.keypoints[30]
         self.all_euclidian()
@@ -26,12 +30,12 @@ class img_keypoints():
         clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(5, 5))
         self.clahe_image = clahe.apply(gray)
         self.faceprime = face_cascade.detectMultiScale(self.clahe_image, 1.3, 5)
-
         if len((self.faceprime)) == 0:
             self.faceprime = [[0, 0, self.img.shape[0], self.img.shape[1]]]
+            self.crop_image()
 
     def crop_image(self):
-        # Convert face coords to rectangle corner points, grow rectangle to capture full face
+        """ Convert face coords to rectangle corner points, grow rectangle to capture full face """
         for (x, y, w, h) in self.faceprime:
             grow = h / 7
             size = (h, w)
@@ -42,30 +46,38 @@ class img_keypoints():
 
         # Save cropped face img
         self.color_normal = self.img[y1:y2, x1:x2]
-        clahe_crop = self.clahe_image[y1:y2, x1:x2]
-        self.equalized_img = cv2.equalizeHist(clahe_crop)
-        self.clahe = clahe_crop
+        ''' 
+        If opencv's Haar detector can't find a shape, it's likely dlib can if we pass along the 
+        image. Otherwise, we use the cropped result from opencv 
+        '''
+        if 0 in self.color_normal.shape:
+            self.color_normal = self.img
+            self.equalized_img = cv2.equalizeHist(self.clahe_image)
+        else:
+            clahe_crop = self.clahe_image[y1:y2, x1:x2]
+            self.equalized_img = cv2.equalizeHist(clahe_crop)
+            self.clahe = clahe_crop
 
-        # cv2.imwrite('clahe_eq.jpg', equalized_img)
-
+        self.write_img('equalized_img', self.equalized_img)
 
     def detect_keypoints(self):
         # Detect face landmarks with dlib rectangle, dlib shape predictor
         detector = dlib.get_frontal_face_detector()
         predictor = dlib.shape_predictor(
-            'shape_predictor_68_face_landmarks.dat')
+            'shape_predictor_68_face_landmarks.dat'
+        )
         dets = detector(self.equalized_img, 1)
         # shape = predictor(self.clahe,1)
         for k, d in enumerate(dets):
-        
+
             # Get the landmarks/parts for the face in box d.
             shape = predictor(self.equalized_img, d)
             # Get the landmarks/parts for the face in box d.
             self.shape = predictor(self.img, d)
 
             self.keypoints = [(shape.part(i).x, shape.part(i).y)
-                            for i in range(shape.num_parts)]
-        
+                              for i in range(shape.num_parts)]
+
         return self.keypoints
 
     def show_keypoints(self):
@@ -76,12 +88,16 @@ class img_keypoints():
                 self.color_normal,
                 self.keypoints[i],
                 1, (0, 235, 235),
-                thickness=2)
-        import os
-        sub = os.getcwd()
-        im_out = sub+'/_DRAW_' + self.img_name
+                thickness=2
+            )
+        self.write_img('draw', self.color_normal)
+
+    def write_img(self, path, image):
+        if not self.draw:
+            return
+        im_out = './' + path + '/' + self.img_name.split('/')[-1]
         print(im_out)
-        cv2.imwrite(im_out, self.color_normal)
+        cv2.imwrite(im_out, image)
 
     def euc_center(self):
 
@@ -114,8 +130,8 @@ class img_keypoints():
     def all_euclidian(self):
         self.euc_xy()
         self.euc_center()
-        self.tensor = np.rot90(np.hstack(
-            (self.euclidianX, self.euclidianY)))  #, self.euclidian)))
+        self.tensor = np.rot90(np.hstack((self.euclidianX, self.euclidianY))
+                              )  #, self.euclidian)))
 
     def distance_1D(self, a, b):
         """ Return x,y distance between 2 coordinate pairs, assistant function to array_maker """
@@ -141,7 +157,24 @@ def test():
     return ex
 
 
-# X = test()
-# print(X.keypoints)
-# X.show_keypoints()
-# print([d.shape for d in [X.euclidianX, X.euclidianY, X.euclidian,X.tensor]])
+def check_detectors():
+    """ Runs as import to check for detector files """
+    import os
+    from urllib.request import urlretrieve
+
+    dl = {
+        'haarcascade_frontalface_alt2.xml':
+            'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_alt2.xml',
+        'shape_predictor_68_face_landmarks.dat':
+            'https://github.com/AKSHAYUBHAT/TensorFace/raw/master/openface/models/dlib/shape_predictor_68_face_landmarks.dat'
+    }
+
+    for k in dl:
+        if not os.path.exists(k):
+            print('downloading', k)
+            urlretrieve(dl[k], k)
+        else:
+            print('found', k)
+
+
+check_detectors()
